84b6c487ec4818034bf1ea0dec434f00e94e8f5d
loctodale <thang336655@gmail.com>
2025-02-08 07:49:20 +0700

    up chat

---
+++ FPT.TeamMatching/FPT.TeamMatching.Domain/Lib/KafkaProducerConfig.cs
@@ -0,0 +1,23 @@
+﻿using Confluent.Kafka;
+
+namespace FPT.TeamMatching.Domain.Lib;
+public interface IKafkaProducerConfig
+{
+    Task ProduceAsync(string topic, Message<string, string> message);
+}
+public class KafkaProducer : IKafkaProducerConfig
+{
+    private readonly IProducer<string, string> _producer;
+    public KafkaProducer()
+    {
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:29092",
+        };
+        _producer = new ProducerBuilder<string, string>(config).Build();
+    }
+    public async Task ProduceAsync(string topic, Message<string, string> message)
+    {
+        await _producer.ProduceAsync(topic, message);
+    }
+}
