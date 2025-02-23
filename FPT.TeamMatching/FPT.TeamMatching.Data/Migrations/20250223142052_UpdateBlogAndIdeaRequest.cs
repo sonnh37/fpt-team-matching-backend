6ab056681a203a6470c91bc331e6a4f2ca220f27
Son Nguyen <nguyenson20zzzz@gmail.com>
2025-02-23 21:22:21 +0700

    Update db blog, ideaRequest

---
+++ FPT.TeamMatching/FPT.TeamMatching.Data/Migrations/20250223142052_UpdateBlogAndIdeaRequest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FPT.TeamMatching.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBlogAndIdeaRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IdeaReview");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Blog");
+
+            migrationBuilder.CreateTable(
+                name: "IdeaRequest",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "gen_random_uuid()"),
+                    IdeaId = table.Column<Guid>(type: "uuid", nullable: true),
+                    ReviewerId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Content = table.Column<string>(type: "text", nullable: true),
+                    ProcessDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "text", nullable: true),
+                    UpdatedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    Note = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdeaRequest", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IdeaRequest_Idea_IdeaId",
+                        column: x => x.IdeaId,
+                        principalTable: "Idea",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_IdeaRequest_User_ReviewerId",
+                        column: x => x.ReviewerId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdeaRequest_IdeaId",
+                table: "IdeaRequest",
+                column: "IdeaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdeaRequest_ReviewerId",
+                table: "IdeaRequest",
+                column: "ReviewerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IdeaRequest");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Blog",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "IdeaReview",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "gen_random_uuid()"),
+                    IdeaId = table.Column<Guid>(type: "uuid", nullable: true),
+                    ReviewerId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    Note = table.Column<string>(type: "text", nullable: true),
+                    ProcessDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "text", nullable: true),
+                    UpdatedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdeaReview", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IdeaReview_Idea_IdeaId",
+                        column: x => x.IdeaId,
+                        principalTable: "Idea",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_IdeaReview_User_ReviewerId",
+                        column: x => x.ReviewerId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdeaReview_IdeaId",
+                table: "IdeaReview",
+                column: "IdeaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdeaReview_ReviewerId",
+                table: "IdeaReview",
+                column: "ReviewerId");
+        }
+    }
+}
