fa91f4b1df242177b3395ce86614b850d817df99
Son Nguyen <nguyenson20zzzz@gmail.com>
2025-03-01 00:05:42 +0700

    Update order in Program.cs

--- FPT.TeamMatching/FPT.TeamMatching.API/Program.cs
+++ FPT.TeamMatching/FPT.TeamMatching.API/Program.cs
@@ -16,8 +16,6 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using MongoDB.Driver;
 using Npgsql;
 
 Env.Load();
@@ -25,34 +23,18 @@ Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddLogging();
 
-#region Hangfire Config
-
-builder.Services.AddHangfire(config =>
+#region Core services
+builder.Services.AddControllers().AddJsonOptions(options =>
 {
-    config.UseSimpleAssemblyNameTypeSerializer()
-        .UseRecommendedSerializerSettings()
-        .UsePostgreSqlStorage(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
 });
-builder.Services.AddHangfireServer();
-
-builder.Services.AddScoped<IJobHangfireService, JobHangFireService>();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 #endregion
 
-#region Add SignalR
-
-builder.Services.AddSignalR();
-
-#endregion
-
-#region Add Kafka Config
-
-builder.Services.AddScoped<IKafkaProducerConfig, KafkaProducer>();
-
-#endregion
-
-#region Add-DbContext
-
+#region Database
 var dbDataSource = new NpgsqlDataSourceBuilder(builder.Configuration.GetConnectionString("DefaultConnection")).Build();
 builder.Services.AddDbContext<FPTMatchingDbContext>(options =>
 {
@@ -60,72 +42,11 @@ builder.Services.AddDbContext<FPTMatchingDbContext>(options =>
         npgsqlOptions => npgsqlOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery));
     options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 });
-
-
-// var mongoClient = new MongoClient(builder.Configuration["MONGODB_URI"]);
-// var database = mongoClient.GetDatabase("fpt-matching-chatroom");
-// builder.Services.AddDbContext<ChatRoomDbContext>(options =>
-// {
-//     options.UseMongoDB(database.Client, database.DatabaseNamespace.DatabaseName);
-// });
-
 builder.Services.AddSingleton<ChatRoomDbContext>();
-
 #endregion
 
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-    // options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-});
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = "bearer"
-    });
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] { }
-        }
-    });
-});
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddAutoMapper(typeof(MappingProfile));
-
-builder.Services.AddCollectionRepositories();
-builder.Services.AddCollectionServices();
-
-#region Cloudinary
-
-builder.Services.AddScoped<CloudinaryConfig>();
-
-#endregion
-
-#region Redis
-
-builder.Services.AddSingleton<RedisConfig>();
-builder.Services.AddSingleton<RedisUtil>();
-#endregion
-
-#region Config-Authentication_Authorization
-
+#region Authentication & Authorization
 builder.Services.Configure<TokenSetting>(builder.Configuration.GetSection("TokenSetting"));
-
 builder.Services.AddAuthentication(x =>
     {
         x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -155,11 +76,10 @@ builder.Services.AddAuthentication(x =>
             }
         };
 
-        // Lấy token từ cookie
         options.Events = new JwtBearerEvents
         {
             OnMessageReceived = context =>
-            { // httpContext
+            {
                 var accessToken = context.Request.Cookies["accessToken"];
                 if (!string.IsNullOrEmpty(accessToken)) context.Token = accessToken;
 
@@ -168,11 +88,9 @@ builder.Services.AddAuthentication(x =>
         };
     });
 builder.Services.AddAuthorization();
-
 #endregion
 
-#region Add-Cors
-
+#region CORS
 builder.Services.AddCors(options =>
 {
     var frontendDomains = builder.Configuration.GetValue<string>("Frontend:Domain")?.Split(',');
@@ -185,33 +103,54 @@ builder.Services.AddCors(options =>
             .AllowAnyMethod();
     });
 });
-
 #endregion
 
+#region SignalR
+builder.Services.AddSignalR();
+#endregion
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+#region Kafka
+builder.Services.AddScoped<IKafkaProducerConfig, KafkaProducer>();
+#endregion
+
+#region Hangfire
+builder.Services.AddHangfire(config =>
+{
+    config.UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UsePostgreSqlStorage(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddHangfireServer();
+builder.Services.AddScoped<IJobHangfireService, JobHangFireService>();
+#endregion
+
+#region Cloudinary
+builder.Services.AddScoped<CloudinaryConfig>();
+#endregion
+
+#region Redis
+builder.Services.AddSingleton<RedisConfig>();
+builder.Services.AddSingleton<RedisUtil>();
+#endregion
+
+#region Custom Repositories & Services
+builder.Services.AddCollectionRepositories();
+builder.Services.AddCollectionServices();
+#endregion 
 
 // -----------------app-------------------------
 
 var app = builder.Build();
 
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors("AllowSpecificOrigins");
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseMiddleware<AuthenticationMiddleware>();
 app.UseSwagger();
 app.UseSwaggerUI();
-app.UseMiddleware<AuthenticationMiddleware>()
-    .UseHttpsRedirection()
-    .UseRouting()
-    .UseCors("AllowSpecificOrigins")
-    .UseAuthentication()
-    .UseAuthorization()
-    .UseHangfireDashboard();
-
+app.UseHangfireDashboard();
 app.MapHub<ChatHub>("/chat");
 app.MapHub<NotificationHub>("/notification");
 app.MapControllers();
