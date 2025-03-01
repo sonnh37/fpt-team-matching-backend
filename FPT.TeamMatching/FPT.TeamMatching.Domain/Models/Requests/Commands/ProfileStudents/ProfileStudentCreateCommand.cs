b67c7f95c103e982d78483654920595966d1e7fd
Son Nguyen <nguyenson20zzzz@gmail.com>
2025-03-01 20:09:37 +0700

    Update model commands

--- FPT.TeamMatching/FPT.TeamMatching.Domain/Models/Requests/Commands/ProfileStudents/ProfileStudentCreateCommand.cs
+++ FPT.TeamMatching/FPT.TeamMatching.Domain/Models/Requests/Commands/ProfileStudents/ProfileStudentCreateCommand.cs
@@ -5,7 +5,11 @@ namespace FPT.TeamMatching.Domain.Models.Requests.Commands.ProfileStudents;
 
 public class ProfileStudentCreateCommand : CreateCommand
 {
-    public Guid UserId { get; set; }
+    public Guid? UserId { get; set; }
+    
+    public Guid? SpecialtyId { get; set; }
+    
+    public Guid? SemesterId { get; set; }
 
     public string? Bio { get; set; }
 
@@ -13,17 +17,11 @@ public class ProfileStudentCreateCommand : CreateCommand
 
     public bool IsQualifiedForAcademicProject { get; set; }
 
-    public string? Major { get; set; }
-
     public string? Achievement { get; set; }
-
-    public string? Semester { get; set; }
 
     public string? ExperienceProject { get; set; }
 
     public string? Interest { get; set; }
 
     public IFormFile? FileCv { get; set; }
-
-    public string? Department { get; set; }
 }
