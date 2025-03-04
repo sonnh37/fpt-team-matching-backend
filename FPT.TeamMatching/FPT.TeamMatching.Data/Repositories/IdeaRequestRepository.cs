26359640f4fbd86dab0e1c25e6da69f3966bbb75
Son Nguyen <nguyenson20zzzz@gmail.com>
2025-03-04 13:15:33 +0700

    Update api idearequest get all by status

--- FPT.TeamMatching/FPT.TeamMatching.Data/Repositories/IdeaRequestRepository.cs
+++ FPT.TeamMatching/FPT.TeamMatching.Data/Repositories/IdeaRequestRepository.cs
@@ -3,6 +3,10 @@ using FPT.TeamMatching.Data.Context;
 using FPT.TeamMatching.Data.Repositories.Base;
 using FPT.TeamMatching.Domain.Contracts.Repositories;
 using FPT.TeamMatching.Domain.Entities;
+using FPT.TeamMatching.Domain.Enums;
+using FPT.TeamMatching.Domain.Models.Requests.Queries.IdeaRequests;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver.Linq;
 
 namespace FPT.TeamMatching.Data.Repositories;
 
@@ -10,5 +14,32 @@ public class IdeaRequestRepository : BaseRepository<IdeaRequest>, IIdeaRequestRe
 {
     public IdeaRequestRepository(FPTMatchingDbContext dbContext) : base(dbContext)
     {
+    }
+
+    public async Task<(List<IdeaRequest>, int)> GetDataByStatus(IdeaRequestGetAllByStatusQuery query)
+    {
+        var queryable = GetQueryable();
+        queryable = queryable.Where(m => m.Status != null && query.StatusList.Contains(m.Status.Value));
+
+        queryable = queryable.Include(m => m.Idea)
+            .Include(m => m.Reviewer);
+
+        if (query.IsPagination)
+        {
+            // Tổng số count sau khi  filter khi chưa lọc trang
+            var totalOrigin = queryable.Count();
+            // Sắp sếp
+            queryable = Sort(queryable, query);
+            // Lọc trang
+            var results = await GetQueryablePagination(queryable, query).ToListAsync();
+
+            return (results, totalOrigin);
+        }
+        else
+        {
+            queryable = Sort(queryable, query);
+            var results = await queryable.ToListAsync();
+            return (results, results.Count);
+        }
     }
 }
