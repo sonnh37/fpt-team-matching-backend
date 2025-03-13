cbbef42f793b4dea00f67a3c6d7dd9b77f0fcfd8
Thanh Thu <thanhthu26082003@gmail.com>
2025-03-13 15:39:09 +0700

    UpdateDatabaseAddMentorIdeaRequest,StageIdea,CapstoneSchedule

---
+++ FPT.TeamMatching/FPT.TeamMatching.Domain/Entities/StageIdea.cs
@@ -0,0 +1,22 @@
+﻿using FPT.TeamMatching.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPT.TeamMatching.Domain.Entities
+{
+    public class StageIdea: BaseEntity
+    {
+        public Guid? SemesterId { get; set; }
+
+        public DateTimeOffset StartDate { get; set; }
+
+        public DateTimeOffset EndDate { get; set; }
+
+        public DateTimeOffset ResultDate { get; set; }
+    
+        public virtual Semester? Semester { get; set; }
+    }
+}
