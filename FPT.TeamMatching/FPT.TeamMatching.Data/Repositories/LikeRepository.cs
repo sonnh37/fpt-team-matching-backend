59967df620fbbe3430c6cb7ce9637d041849d4f3
Thanh Thu <thanhthu26082003@gmail.com>
2025-03-21 14:10:29 +0700

    API delete like by blogId

--- FPT.TeamMatching/FPT.TeamMatching.Data/Repositories/LikeRepository.cs
+++ FPT.TeamMatching/FPT.TeamMatching.Data/Repositories/LikeRepository.cs
@@ -2,12 +2,33 @@
 using FPT.TeamMatching.Data.Repositories.Base;
 using FPT.TeamMatching.Domain.Contracts.Repositories;
 using FPT.TeamMatching.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace FPT.TeamMatching.Data.Repositories;
 
 public class LikeRepository : BaseRepository<Like>, ILikeRepository
 {
+    private readonly FPTMatchingDbContext _dbContext;
     public LikeRepository(FPTMatchingDbContext dbContext) : base(dbContext)
     {
+        _dbContext = dbContext;
+    }
+
+    public async Task<bool> DeleteLikeByBlogId(Guid blogId, Guid userId)
+    {
+        var like = await _dbContext.Likes
+            .Where(e => e.IsDeleted == false &&
+                        e.BlogId == blogId &&
+                        e.UserId == userId)
+            .FirstOrDefaultAsync();
+
+        if (like != null)
+        {
+            _dbContext.Likes.Remove(like);
+            var result = await _dbContext.SaveChangesAsync();
+            return result > 0;
+        }
+
+        return false;
     }
 }
--- FPT.TeamMatching/FPT.TeamMatching.Services/LikeService.cs
+++ FPT.TeamMatching/FPT.TeamMatching.Services/LikeService.cs
@@ -3,16 +3,50 @@ using FPT.TeamMatching.Domain.Contracts.Repositories;
 using FPT.TeamMatching.Domain.Contracts.Services;
 using FPT.TeamMatching.Domain.Contracts.UnitOfWorks;
 using FPT.TeamMatching.Domain.Entities;
+using FPT.TeamMatching.Domain.Models.Responses;
+using FPT.TeamMatching.Domain.Models.Results;
+using FPT.TeamMatching.Domain.Utilities;
 using FPT.TeamMatching.Services.Bases;
 
 namespace FPT.TeamMatching.Services;
 
 public class LikeService : BaseService<Like>, ILikeService
 {
-    private readonly ILikeRepository _service;
+    private readonly ILikeRepository _likeRepository;
 
     public LikeService(IMapper mapper, IUnitOfWork unitOfWork) : base(mapper, unitOfWork)
     {
-        _service = _unitOfWork.LikeRepository;
+        _likeRepository = _unitOfWork.LikeRepository;
+    }
+
+    public async Task<BusinessResult> DeleteLikeByBlogId(Guid blogId)
+    {
+        try
+        {
+            var userId = GetUserIdFromClaims();
+            if (userId != null)
+            {
+                var isSuccess = await _likeRepository.DeleteLikeByBlogId(blogId, (Guid)userId);
+                if (isSuccess)
+                {
+                    return new ResponseBuilder()
+                    .WithStatus(Const.SUCCESS_CODE)
+                    .WithMessage(Const.SUCCESS_DELETE_MSG);
+                }
+                return new ResponseBuilder()
+                .WithStatus(Const.FAIL_CODE)
+                .WithMessage(Const.FAIL_DELETE_MSG);
+            }
+            return new ResponseBuilder()
+                .WithStatus(Const.FAIL_CODE)
+                .WithMessage(Const.FAIL_READ_MSG);
+        }
+        catch (Exception ex)
+        {
+            var errorMessage = $"An error {typeof(IdeaResult).Name}: {ex.Message}";
+            return new ResponseBuilder()
+                .WithStatus(Const.FAIL_CODE)
+                .WithMessage(errorMessage);
+        }
     }
 }
