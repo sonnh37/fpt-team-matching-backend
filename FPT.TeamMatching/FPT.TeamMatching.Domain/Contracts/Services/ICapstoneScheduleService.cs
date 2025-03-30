7a73d9177eb5d86eca06b46525c349c5a7b71c71
Thanh Thu <thanhthu26082003@gmail.com>
2025-03-30 16:41:50 +0700

    API Get CapstoneSchedule by SemesterId and Stage

--- FPT.TeamMatching/FPT.TeamMatching.Domain/Contracts/Services/ICapstoneScheduleService.cs
+++ FPT.TeamMatching/FPT.TeamMatching.Domain/Contracts/Services/ICapstoneScheduleService.cs
@@ -6,11 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using FPT.TeamMatching.Domain.Models.Responses;
 using Microsoft.AspNetCore.Http;
+using FPT.TeamMatching.Domain.Models.Requests.Commands.CapstoneSchedules;
 
 namespace FPT.TeamMatching.Domain.Contracts.Services
 {
     public interface ICapstoneScheduleService: IBaseService
     {
         Task<BusinessResult> ImportExcelFile(IFormFile file, int stage);
+        Task<BusinessResult> GetBySemesterIdAndStage(CapstoneScheduleFilter command);
     }
 }
---
+++ FPT.TeamMatching/FPT.TeamMatching.Domain/Models/Requests/Commands/CapstoneSchedules/CapstoneScheduleFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPT.TeamMatching.Domain.Models.Requests.Commands.CapstoneSchedules
+{
+    public class CapstoneScheduleFilter
+    {
+        public Guid SemesterId {  get; set; }
+        public int Stage {  get; set; }
+    }
+}
