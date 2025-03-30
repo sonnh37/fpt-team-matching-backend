20cdfca5d0b25c50ad2bf913ff128309a0f1efd6
loctodale <thang336655@gmail.com>
2025-03-30 11:07:30 +0700

    add upload file capstone schedule

--- FPT.TeamMatching/FPT.TeamMatching.Domain/Contracts/Services/ICapstoneScheduleService.cs
+++ FPT.TeamMatching/FPT.TeamMatching.Domain/Contracts/Services/ICapstoneScheduleService.cs
@@ -4,10 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FPT.TeamMatching.Domain.Models.Responses;
+using Microsoft.AspNetCore.Http;
 
 namespace FPT.TeamMatching.Domain.Contracts.Services
 {
     public interface ICapstoneScheduleService: IBaseService
     {
+        Task<BusinessResult> ImportExcelFile(IFormFile file, int stage);
     }
 }
