f2447c31a3ccbdf4665188f11019e8d229c22ab5
loctodale <thang336655@gmail.com>
2025-04-04 20:29:28 +0700

    update dependency include mapping

--- FPT.TeamMatching/FPT.TeamMatching.API/Collections/CollectionRepositories.cs
+++ FPT.TeamMatching/FPT.TeamMatching.API/Collections/CollectionRepositories.cs
@@ -40,5 +40,6 @@ public static class CollectionRepositories
         services.AddScoped<IStageIdeaRepositoty, StageIdeaRepository>();
         services.AddScoped<ICapstoneScheduleRepository, CapstoneScheduleRepository>();
         services.AddScoped<IMentorIdeaRequestRepository, MentorIdeaRequestRepository>();
+        services.AddScoped<IMentorFeedbackRepository, MentorFeedbackRepository>();
     }
 }
