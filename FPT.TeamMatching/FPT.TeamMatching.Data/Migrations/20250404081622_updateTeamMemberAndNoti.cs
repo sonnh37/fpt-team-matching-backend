d79e0086bb9ed48d24f511d8f0fd2a4426bce91c
Thanh Thu <thanhthu26082003@gmail.com>
2025-04-04 15:18:19 +0700

    update db

---
+++ FPT.TeamMatching/FPT.TeamMatching.Data/Migrations/20250404081622_updateTeamMemberAndNoti.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FPT.TeamMatching.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateTeamMemberAndNoti : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Attitude",
+                table: "TeamMember",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProjectId",
+                table: "Notification",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notification_ProjectId",
+                table: "Notification",
+                column: "ProjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notification_Project_ProjectId",
+                table: "Notification",
+                column: "ProjectId",
+                principalTable: "Project",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notification_Project_ProjectId",
+                table: "Notification");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notification_ProjectId",
+                table: "Notification");
+
+            migrationBuilder.DropColumn(
+                name: "Attitude",
+                table: "TeamMember");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectId",
+                table: "Notification");
+        }
+    }
+}
