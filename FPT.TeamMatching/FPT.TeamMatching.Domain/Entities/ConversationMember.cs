d45ff1b95d7bfe90a2595c1c0cba583af3290f2b
loctodale <thang336655@gmail.com>
2025-04-24 17:38:38 +0700

    drop table conversation conversationmember messages

--- FPT.TeamMatching/FPT.TeamMatching.Domain/Entities/ConversationMember.cs
+++ FPT.TeamMatching/FPT.TeamMatching.Domain/Entities/ConversationMember.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using FPT.TeamMatching.Domain.Models.Results;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace FPT.TeamMatching.Domain.Entities;
 
@@ -7,6 +8,12 @@ public class ConversationMember
     [BsonId] public string Id { get; set; } = Guid.NewGuid().ToString();
 
     [BsonElement] public string? UserId { get; set; }
+    
+    [BsonElement] public string? AvatarUrl { get; set; }
+    
+    [BsonElement] public string? Code { get; set; }
+    
+    [BsonElement] public ICollection<UserXRoleResult>? Role { get; set; }
 
     [BsonElement("conversationId")] public string? ConversationId { get; set; }
 
