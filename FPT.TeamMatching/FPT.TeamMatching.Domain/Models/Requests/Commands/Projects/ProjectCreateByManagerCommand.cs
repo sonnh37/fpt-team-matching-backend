6101ad5bc3be033d34003478b3510c68b5d430f9
loctodale <thang336655@gmail.com>
2025-05-20 02:00:14 +0700

    update project create by manager

--- FPT.TeamMatching/FPT.TeamMatching.Domain/Models/Requests/Commands/Projects/ProjectCreateByManagerCommand.cs
+++ FPT.TeamMatching/FPT.TeamMatching.Domain/Models/Requests/Commands/Projects/ProjectCreateByManagerCommand.cs
@@ -5,6 +5,7 @@ namespace FPT.TeamMatching.Domain.Models.Requests.Commands.Projects;
 public class ProjectCreateByManagerCommand
 {
     public Guid? LeaderId { get; set; }
+    public Guid? TopicId { get; set; }
 
     public ICollection<TeamMemberCreateCommand>? TeamMembers { get; set; }
 }
